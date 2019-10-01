5e23b6d441f13ef04b9c2445b526d3a0ecce77a4
dop3ch3f <ifeanyi.ibekie@gmail.com>
2019-10-01 22:36:12 +0100

    completed: - parse Contacts function. - signup function, - update function, - delete function, - status check function, - sync single chat with conversations function.

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,41 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const mongodb = require('mongodb');
 const express = require('express');
 const bodyParser = require("body-parser");
+const cors = require('cors');
 admin.initializeApp(functions.config().firebase);
-let db = admin.firestore();
-let usersRef = db.collection('users');
-let contactsRef = db.collection('contacts');
+const db = admin.firestore();
+const usersRef = db.collection('users');
+const contactsRef = db.collection('contacts');
+const messageRef = db.collection('messages');
+const chatsRef = db.collection('chats');
 
 
 const app = express();
 const main = express();
 
+main.use(cors());
 main.use('/api/v1', app);
 main.use(bodyParser.json());
 
-const parseContacts = async (contacts) => {
+// contacts and db parser function
+const parseContacts = async (contacts, user_id) => {
     try {
+        const user_info = await usersRef.doc(user_id).get();
+        // array holder for final contact values
         const newContacts = [];
-        const queries = contacts.map((f) => usersRef.where('phone', '<=', f.phone).limit(1).get());
+        // create query for each contact
+        const queries = contacts.map((f) => usersRef.where('generatedPhones', 'array-contains', f.phone).limit(1).get());
+        // dispatch all queries concurrently
         const request = await Promise.all(queries);
-        request.map((doc) => {
+        // create new contact instance and push to holder variable
+        request.map((doc, i) => {
             if (doc.empty) return null;
+            if (user_info.data().generatedPhones.includes(doc.docs[0].data().phone)) return null;
             return newContacts.push({
                 avatar: doc.docs[0].data().avatar || "",
-                display_name: doc.docs[0].data().username || "",
+                display_name: contacts[i].display_name || doc.docs[0].data().username,
                 email: doc.docs[0].data().email || "",
                 ecodeem_id: doc.docs[0].data().ecodeem_id || "",
                 phone: doc.docs[0].data().phone || "",
@@ -37,29 +49,28 @@ const parseContacts = async (contacts) => {
 
 }
 
+// route to parse contacts
 app.post('/parseContacts', async (req, res) => {
     try {
-        // read contacts
         const { contacts, user_id } = req.body;
-
-        const parsedContacts = await parseContacts(contacts);
+        // prepare new contacts function
+        const parsedContacts = await parseContacts(contacts, user_id);
         if (parsedContacts.length > 0) {
+            // delete all old contacts registered for a user
             await contactsRef.where("user_id", '==', user_id).get().then((querySnapshot) => {
                 return querySnapshot.forEach(async (doc) => {
-                    doc.data
-                    return await doc.ref.delete();
+                    await doc.ref.delete();
                 });
             });
+            // create new instances for already prepared contacts
             await parsedContacts.forEach(async (contact_info) => {
-                console.log(user_id, JSON.stringify(contact_info));
                 await contactsRef.add({
                     user_id: user_id,
-                    contact_information: contact_info,
+                    ...contact_info,
                 });
             });
         }
-        // fetch users that match each contact
-        // update to user object of his contacts
+        // final response
         return res.json({
             success: true,
             message: "processing initiated"
@@ -73,6 +84,102 @@ app.post('/parseContacts', async (req, res) => {
     }
 });
 
+// route to signup new user
+app.post("/signup", async (req, res) => {
+    try {
+        const { country, ecodeem_id, email, phone, username, avatar, generatedPhones } = req.body;
+        await usersRef.doc(ecodeem_id).set({
+            conversations: [],
+            avatar: avatar,
+            country: country,
+            ecodeem_id: ecodeem_id,
+            email: email,
+            last_seen: Date.now(),
+            online: true,
+            phone: phone,
+            username: username,
+            generatedPhones: generatedPhones
+        });
+        return res.json({
+            success: true,
+            message: "user created"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            success: false,
+            error: error
+        });
+    }
+});
+
+// route to update user details
+app.put("/update", async (req, res) => {
+    try {
+        const { country, ecodeem_id, email, phone, username, avatar } = req.body;
+        await usersRef.doc(ecodeem_id).update({
+            ecodeem_id: ecodeem_id,
+            avatar: avatar,
+            country: country,
+            email: email,
+            last_seen: Date.now(),
+            online: true,
+            phone: phone,
+            username: username,
+        });
+        return res.json({
+            success: true,
+            message: "user updated"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            success: false,
+            error: error
+        })
+    }
+});
+
+// route to delete existing user
+app.delete("/delete/:ecodeem_id", async (req, res) => {
+    try {
+        const { ecodeem_id } = req.params;
+        await usersRef.doc(ecodeem_id).delete();
+        return res.json({
+            success: true,
+            message: "user deleted"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            success: false,
+            error: error
+        })
+    }
+});
+
+//route to switch status
+app.get("/users/:user_id/status/:status", async (req, res) => {
+    try {
+        const { user_id, status } = req.params;
+        await usersRef.doc(user_id).update({
+            online: status === 'online' ? true : false,
+            last_seen: new Date().now()
+        });
+        return res.json({
+            success: true,
+            message: 'user status changed'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            success: false,
+            error: error
+        });
+    }
+})
+
+// route for status check on function
 app.get('/', async (req, res) => {
     res.json({
         success: true,
@@ -81,3 +188,40 @@ app.get('/', async (req, res) => {
 })
 
 exports.webApi = functions.https.onRequest(main);
+
+
+const prepareLastMessage = (message_data) => {
+    switch (message_data.type) {
+        case "text":
+            return message_data.text;
+        case "image":
+            return "Image received";
+        case "video":
+            return "Video received";
+        case "post":
+            return "Ecodeem post received";
+        default:
+            return "Message received";
+    }
+}
+
+// listener for chat changes to update user conversations
+exports.syncSingleChatWithConversations = functions.firestore.document('message/{message_id}').onCreate((change, context) => {
+    const chat_id = change.data().chat_id;
+    const type = change.data().type;
+    if (type === 'single') {
+        const usersArray = chat_id.split("-");
+        usersArray.forEach(async (user, index) => {
+            const otherUser = await usersRef.doc(usersArray[index === 0 ? 1 : 0]).get();
+            await usersRef.doc(user).update({
+                conversations: admin.firestore.FieldValue.arrayUnion({
+                    chat_id: chat_id,
+                    user_id: usersArray[index === 0 ? 1 : 0],
+                    date: new Date().now(),
+                    last_message: prepareLastMessage(change.data()),
+                }),
+            });
+        });
+    }
+
+});
